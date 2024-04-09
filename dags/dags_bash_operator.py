b1aa39200fde18bf7df7af9bec80cdb7fa2a33ff
KongjuRa <kjra@moramcnt.com>
2024-04-09 11:17:50 +0900

    dags commit

---
+++ dags/dags_bash_operator.py
@@ -0,0 +1,23 @@
+from airflow.models.dag import DAG
+import datetime
+import pendulum
+from airflow.operators.bash import BashOperator
+
+# 모든 DAG은 정의가 필요하다.
+with DAG(  
+    dag_id="dags_bash_operator",
+    schedule="0 0 * * *",
+    start_date=pendulum.datetime(2023, 3, 1, tz="Asia/Seoul"),
+    catchup=False,
+) as dag:
+    basg_t1 = BashOperator(
+        task_id="bash_t1",
+        bash_command="echo whoami",
+    )
+
+    basg_t2 = BashOperator(
+        task_id="bash_t2",
+        bash_command="echo $HOSTNAME",
+    )
+
+    basg_t1 >> basg_t2
