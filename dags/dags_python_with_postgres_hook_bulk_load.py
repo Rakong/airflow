820c28a2de2e453be0412b0c8c77e1bf89e772aa
KongjuRa <kjra@moramcnt.com>
2024-04-16 15:24:12 +0900

    0416 comment추가

--- dags/dags_python_with_postgres_hook_bulk_load.py
+++ dags/dags_python_with_postgres_hook_bulk_load.py
@@ -2,7 +2,21 @@ from airflow import DAG
 import pendulum
 from airflow.operators.python import PythonOperator
 from airflow.providers.postgres.hooks.postgres import PostgresHook
-
+"""
+    bulk_load(table, tmp_file)
+    Bulk_load 문제점 & 개선방안
+    
+    문제점
+    (1) Load 가능한 Delimiter는 Tab으로 고정되어 있음(,일경우 tab으로 변경을 해줘야함 %s/,/\t/g)
+    (2) Header까지 포함해서 업로드됨
+    (3) 특수문자로 인해 파싱이 안될 경우 에러 발생(^M)
+    
+    개선방안
+    (1) Custom Hook 을 만들어서 Delimiter 유형을 입력받게 하고
+    (2) Header 여부를 선택하게끔 하며
+    (3) 특수문자를 제거하는 로직을 추가 후
+    (4) sqlalchemy를 이용하여 Load 한다면? 그리고 테이블을 생성하면서 업로드할 수 있다면?
+"""
 with DAG(
         dag_id='dags_python_with_postgres_hook_bulk_load',
         start_date=pendulum.datetime(2023, 4, 1, tz='Asia/Seoul'),
--- plugins/hooks/custom_postgres_hook.py
+++ plugins/hooks/custom_postgres_hook.py
@@ -1,22 +1,28 @@
 from airflow.hooks.base import BaseHook
 import psycopg2
 import pandas as pd
-
+"""
+    BaseHook 상속
+    1.def get_conn(self) 구현해줘야함
+    => DB와의 연결 세션 객체인 conn을 리턴하도록 구현
+    2.bulk_load 메서드 구현 
+"""
 class CustomPostgresHook(BaseHook):
 
     def __init__(self, postgres_conn_id, **kwargs):
         self.postgres_conn_id = postgres_conn_id
 
     def get_conn(self):
-        airflow_conn = BaseHook.get_connection(self.postgres_conn_id)
+        airflow_conn = BaseHook.get_connection(self.postgres_conn_id) # airflow connection에 둥록된 정보가 담긴 객체
         self.host = airflow_conn.host
         self.user = airflow_conn.login
         self.password = airflow_conn.password
         self.dbname = airflow_conn.schema
         self.port = airflow_conn.port
 
+        # psycopg2.connet() DB와의 연결을 담은 session 정보가 담긴 객체를 리턴
         self.postgres_conn = psycopg2.connect(host=self.host, user=self.user, password=self.password, dbname=self.dbname, port=self.port)
-        return self.postgres_conn
+        return self.postgres_conn # postgres에 대한 connection 정보
 
     def bulk_load(self, table_name, file_name, delimiter: str, is_header: bool, is_replace: bool):
         from sqlalchemy import create_engine
@@ -27,6 +33,7 @@ class CustomPostgresHook(BaseHook):
         header = 0 if is_header else None                       # is_header = True면 0, False면 None
         if_exists = 'replace' if is_replace else 'append'       # is_replace = True면 replace, False면 append
         file_df = pd.read_csv(file_name, header=header, delimiter=delimiter)
+        # pandas read_csv ( csv -> data )
 
         for col in file_df.columns:                             
             try:
