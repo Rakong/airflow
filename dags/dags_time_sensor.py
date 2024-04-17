7b7aa986033c4270a7a1334e225d66ec13102d54
KongjuRa <kjra@moramcnt.com>
2024-04-17 15:57:18 +0900

    time chng

--- dags/dags_time_sensor.py
+++ dags/dags_time_sensor.py
@@ -4,8 +4,8 @@ from airflow.sensors.date_time import DateTimeSensor
 
 with DAG(
     dag_id="dags_time_sensor",
-    start_date=pendulum.datetime(2023, 5, 1, 0, 0, 0),
-    end_date=pendulum.datetime(2023, 5, 1, 1, 0, 0),
+    start_date=pendulum.datetime(2024, 4, 17, 0, 0, 0),
+    end_date=pendulum.datetime(2024, 4, 17, 1, 0, 0),
     schedule="*/10 * * * *",
     catchup=True,
 ) as dag:
